ebe202f6caaa748110e5e953e9ff9ff04022d8de
Lalit Chaudhary <mail.lalitchaudhary@gmail.com>
2025-09-29 21:00:44 +0530

    Commited

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
+# trunk-ignore(tflint/terraform_required_version)
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-india"  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    storage_account_name = "stgindia"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
